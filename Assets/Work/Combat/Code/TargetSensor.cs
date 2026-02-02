0b868d71c5ab107cb2c30cb5483877c38323a1bd
gominsu08 <minsu080618@gmail.com>
2026-02-02 19:19:29 +0900

    feat : enemyAttack

---
+++ Assets/Work/Combat/Code/TargetSensor.cs
@@ -0,0 +1,68 @@
+﻿using Alchemy.Inspector;
+using System.Collections.Generic;
+using UnityEngine;
+using Work.Entities;
+
+namespace Work.Combat.Code
+{
+    public enum TargetingType
+    {
+        SingleEnemy,
+        AreaEnemy,
+    }
+
+    public enum TargetingShape
+    {
+        Circle,
+        Box,
+    }
+
+    public class TargetSensor : MonoBehaviour
+    {
+        [SerializeField] private TargetingType targetingType = TargetingType.AreaEnemy;
+        [SerializeField] private TargetingShape targetingShape = TargetingShape.Circle;
+
+        private bool IsSingleTarget => targetingType == TargetingType.SingleEnemy;
+        private bool IsAreaTarget => targetingType == TargetingType.AreaEnemy;
+        private bool IsBoxShape => targetingShape == TargetingShape.Box;
+        private bool IsCircleShape => targetingShape == TargetingShape.Circle;
+
+        //여러마리
+        [SerializeField] private LayerMask targetLayer;
+
+        //박스형
+        [ShowIf(nameof(IsBoxShape))]
+        [SerializeField] private Vector3 boxSize = Vector3.one;
+
+        //원형
+        [ShowIf(nameof(IsCircleShape))]
+        [SerializeField] private float circleRadius = 0.5f;
+
+
+
+        public List<IDamageable> Cast()
+        {
+            List<IDamageable> damageables = new List<IDamageable>();
+            Collider[] cols = new Collider[0];
+
+            cols = targetingShape switch
+            {
+                TargetingShape.Box => Physics.OverlapBox(transform.position, boxSize, transform.rotation, targetLayer),
+                TargetingShape.Circle => Physics.OverlapSphere(transform.position, circleRadius, targetLayer),
+                _=> new Collider[0],
+            };
+
+            foreach (Collider col in cols)
+            {
+                IDamageable damageable = col.GetComponent<IDamageable>();
+                if (damageable != null)
+                {
+                    damageables.Add(damageable);
+                }
+            }
+
+            return damageables;
+        }
+
+    }
+}
--- Assets/Work/Enemies/Code/EnemyAttackModule.cs
+++ Assets/Work/Enemies/Code/EnemyAttackModule.cs
@@ -1,9 +1,34 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Work.Combat.Code;
+using Work.Entities;
 
 namespace Work.Enemies.Code
 {
-    public class EnemyAttackModule : MonoBehaviour
+    public class EnemyAttackModule : MonoBehaviour, IEnemyModule
     {
+        private Enemy _owner;
+
+        //아래 두개는 나중에 하나로 묶을예정 지금은 테스트용
+        [SerializeField] private float attackRange;
+        [SerializeField] private float damage;
+
+        [SerializeField] private TargetSensor targetSensor;
+
+        //[SerializeField] private 
+
+        public void Initialize(Enemy enemy)
+        {
+            _owner = enemy;
+        }
+
+        public void Attack()
+        {
+            List<IDamageable> damageables = targetSensor.Cast();
+
+            //공격로직 구현
+            Debug.Log($"{_owner.name} attacks with {damage} damage within {attackRange} range.");
+        }
 
     }
 }
