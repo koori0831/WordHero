82f8ce77462f92e441b050660152d293677afb12
gominsu08 <minsu080618@gmail.com>
2026-03-31 00:37:40 +0900

    fix : Knockback

---
+++ Assets/Work/Shops/Code/DoorOpener.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Work.Core.Utils.EventBus;
+using Work.Stages.Code;
+
+namespace Work.Shops.Code
+{
+    public class DoorOpener : MonoBehaviour
+    {
+        private bool _isOpen = false;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player") && !_isOpen)
+            {
+                _isOpen = true;
+                Bus<StageClearEvent>.Raise(new StageClearEvent());
+            }
+        }
+    }
+}
