ff9dafc66c8581bee467265319261c8e7e13afe8
Gwamegi <minsu080618@gmail.com>
2026-04-28 23:47:03 +0900

    Reapply "feat(Work.Enemies): Initialize enemy variables and settings"
    This reverts commit 5fa639d08f9d6bc946c1176ab0de8bc81269b785.

--- Assets/Work/Enemies/Code/PatrolEnemy.cs
+++ Assets/Work/Enemies/Code/PatrolEnemy.cs
@@ -7,11 +7,14 @@ namespace Work.Enemies.Code
         [SerializeField] protected float patrolRange = 5f;
         [SerializeField] protected int patrolPointCount = 3;
 
-        public override void VariableSetting()
+        public override bool VariableSetting()
         {
-            base.VariableSetting();
+            if (base.VariableSetting() == false)
+                return false;
+
             SetBlackboardVariable<float>(BTVariables.PatrolRange, patrolRange);
             SetBlackboardVariable<int>(BTVariables.PatrolPointCount, patrolPointCount);
+            return true;
         }
     }
 }
